b17a4c66e26a5979d55570c8557bc6e41ae457a1
dell <abdelrhmaninsta1@gmail.com>
2025-09-04 16:46:56 +0300

    dev init

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ void main(){
 class Myapp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return MaterialApp
+    return MaterialApp(
 
       debugShowCheckedModeBanner: false,
       initialRoute: Homescreen.home,
